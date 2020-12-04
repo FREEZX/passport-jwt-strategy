e38fe807eb5cf27a88837faa2d64d30945a50bcf
stormwin <ketejadam@hotmail.com>
2020-12-04 14:40:11 +0100

    Better d.ts definition

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,23 @@
-import { Strategy } from "passport-strategy";
+import * as express from 'express';
 
-declare interface StrategyOptions {
-	secret: string
-	maxAge?: number
-	algorithms?: 'RS256' | 'RS384' | 'RS512' | 'ES256' | 'ES384' | 'ES512' | 'HS256' | 'HS384' | 'HS512';
-	requestKey?: string;
-	requestArg?: string;
+declare namespace JwtStrategy {
+    interface StrategyStatic {
+        new(options: StrategyOptions): StrategyInstance;
+    }
+
+    interface StrategyInstance {
+        name: string;
+        authenticate: (req: express.Request, options?: any) => void;
+    }
+
+    interface StrategyOptions {
+		secret: string
+		maxAge?: number
+		algorithms?: 'RS256' | 'RS384' | 'RS512' | 'ES256' | 'ES384' | 'ES512' | 'HS256' | 'HS384' | 'HS512';
+		requestKey?: string;
+		requestArg?: string;
+	}
 }
 
-declare class JwtStrategy extends Strategy {
-	constructor(StrategyOptions);
-}
+declare const JwtStrategy: JwtStrategy.StrategyStatic;
+export = JwtStrategy;
